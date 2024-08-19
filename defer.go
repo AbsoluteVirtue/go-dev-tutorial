2eceaadd64690e1e6045c93c132ec30a1e9a52f1
AbsoluteVirtue <3hitemouse@gmail.com>
2024-08-19 18:35:01 +0300

    Create defer.go

---
+++ defer.go
@@ -0,0 +1,45 @@
+// https://go.dev/blog/defer-panic-and-recover
+// The function g takes the int i, and panics if i is greater than 3, or else it calls itself with the argument i+1. The function f defers a function that calls recover and prints the recovered value (if it is non-nil).
+// For a real-world example of panic and recover, see the json package from the Go standard library: https://pkg.go.dev/encoding/json
+package main
+
+import "fmt"
+
+func main() {
+    f()
+    fmt.Println("Returned normally from f.")
+}
+
+func f() {
+    defer func() {
+        if r := recover(); r != nil {
+            fmt.Println("Recovered in f", r)
+        }
+    }()
+    fmt.Println("Calling g.")
+    g(0)
+    fmt.Println("Returned normally from g.")
+}
+
+func g(i int) {
+    if i > 3 {
+        fmt.Println("Panicking!")
+        panic(fmt.Sprintf("%v", i))
+    }
+    defer fmt.Println("Defer in g", i)
+    fmt.Println("Printing in g", i)
+    g(i + 1)
+}
+
+// Calling g.
+// Printing in g 0
+// Printing in g 1
+// Printing in g 2
+// Printing in g 3
+// Panicking!
+// Defer in g 3
+// Defer in g 2
+// Defer in g 1
+// Defer in g 0
+// Recovered in f 4
+// Returned normally from f.
